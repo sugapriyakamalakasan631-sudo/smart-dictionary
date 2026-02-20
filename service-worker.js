910591461d04590f054a65190ba969b132597508
sugapriyakamalakasan631-sudo <sugapriyakamalakasan631@gmail.com>
2026-02-20 08:57:24 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('dictionary-cache').then(function(cache) {
+      return cache.addAll(['index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
